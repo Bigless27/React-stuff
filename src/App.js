0104efc6c6852e8271d1a664bb32f73ea87caa92
bigless27 <lessonryan@gmail.com>
2018-12-09 15:43:12 -0600

    complete password reset

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import DashboardPage from "./components/pages/DashboardPage";
 import SignupPage from "./components/pages/SignupPage";
 import UserRoute from './components/routes/UserRoute';
 import GuestRoute from './components/routes/GuestRoute';
+import ForgotPasswordPage from './components/pages/ForgotPasswordPage';
 import ConfirmationPage from "./components/pages/ConfirmationPage";
+import ResetPasswordPage from "./components/pages/ResetPasswordPage";
 
 const App = ({ location }) => (
   <div className="ui container">
@@ -15,6 +17,8 @@ const App = ({ location }) => (
     <Route location={location} path="/confirmation/:token" exact component={ConfirmationPage} />
     <GuestRoute location={location} path="/login" component={LoginPage} />
     <GuestRoute location={location} path="/signup" component={SignupPage} />
+    <GuestRoute location={location} path="/forgot_password" component={ForgotPasswordPage} />
+    <GuestRoute location={location} path="/reset_password/:token" component={ResetPasswordPage} />
     <UserRoute location={location} path="/dashboard" component={DashboardPage} />
   </div>
 );
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import LoginForm from "../forms/LoginForm";
 import { login } from "../../actions/auth";
 
 class LoginPage extends React.Component {
-    submit = data => 
+    submit = data =>
         this.props.login(data).then(() => this.props.history.push('/dashboard'))
-    
+
     render() {
         return (
             <div>
                 <h1>Login page</h1>
 
                 <LoginForm submit={this.submit} />
+
+                <Link to="/forgot_password">Forgot Password?</Link>
             </div>
         );
     };
